7d4d23a357f6102b8506d3af0845f636af2a0326
VIN <71225264+VIN-y@users.noreply.github.com>
2024-06-29 11:38:04 +0100

    Update config_switch.py

--- klipper/extras/config_switch.py
+++ klipper/extras/config_switch.py
@@ -16,8 +16,8 @@ class ConfigSwitch:
         home_dir = os.path.expanduser("~")
 
         config_dir = os.path.join(home_dir, "printer_data/config/config1/")
-        if not os.path.exists(destination_dir):
-            os.makedirs(destination_dir)  
+        # if not os.path.exists(destination_dir):
+        #     os.makedirs(destination_dir)  
 
         # printer_config = os.path.join(home_dir, "printer_data/config/printer.cfg")
 
