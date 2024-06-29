bb4609405947904ecaea4ec4b06535df15305e14
VIN <71225264+VIN-y@users.noreply.github.com>
2024-06-29 21:57:34 +0100

    Update config_switch.py

--- klipper/extras/config_switch.py
+++ klipper/extras/config_switch.py
@@ -15,13 +15,13 @@ class ConfigSwitch:
     def cmd_SAVE_CONFIG_MODE(self, gcmd):
         ## Variables
         home_dir = os.path.expanduser("~")
-        record = False
         destination = ""
+        record = False
 
         printer_config = os.path.join(home_dir, "printer_data/config/printer.cfg")
-        config_dir = os.path.join(home_dir, "printer_data/config/config-test/")
-        tc_config_multi_file = os.path.join(config_dir, "tc_config_multi.cfg")
-        tc_config_single_file = os.path.join(config_dir, "tc_config_single.cfg")
+        config_dir = os.path.join(home_dir, "printer_data/config/config/")
+        config_multi = os.path.join(config_dir, "config_multi.cfg")
+        config_single = os.path.join(config_dir, "config_single.cfg")
 
         ## Make the config folder, if it is not already there
         if not os.path.exists(config_dir):
@@ -29,32 +29,37 @@ class ConfigSwitch:
         
         with open(printer_config) as file:
             for line in file:
-                ## set destination file
+                ## Set destination file
                 if "variable_dock:" in line.strip():
                     if "True" in line.strip():
-                        destination = tc_config_multi_file
+                        destination = config_multi
+                        with open(destination, 'w'):
+                            pass
                     elif "False" in line.strip():
-                        destination = tc_config_single_file
+                        destination = config_single
+                        with open(destination, 'w'):
+                            pass
                     else:
                         raise gcmd.error("[variable_dock: ] must be 'True' or 'False'")
                 
         with open(printer_config) as file:
             if destination != "":
-                self.gcode.respond_info(destination)
-                with open(destination, 'w'):
-                    pass
-
-            for line in file:
-                ## Record point begin / end
-                if "#;<" in line.strip():
-                    record = True
-                if "#;>" in line.strip():
-                    record = False
+                for line in file:
+                    ## Record point begin / end
+                    if "#;<" in line.strip():
+                        record = True
+                    if "#;>" in line.strip():
+                        record = False
+                    
+                    ## Start / Stop record
+                    if record is True:
+                        with open(destination, 'a') as savefile:
+                            savefile.write(line)
                 
-                ## Start / Stop record
-                if record is True:
-                    with open(destination, 'a') as savefile:
-                        savefile.write(line)
+                if "config_multi" in destination:
+                    self.gcode.respond_info("Section variables saved to config/config_multi.cfg")
+                elif "config_single" in destination:
+                    self.gcode.respond_info("Section variables saved to config/config_single.cfg")
 
 
 def load_config(config):
