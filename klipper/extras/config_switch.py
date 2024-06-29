feb1aed209ccc0ed8ce547b9f76e1aba196e0171
VIN <71225264+VIN-y@users.noreply.github.com>
2024-06-29 11:50:42 +0100

    Update config_switch.py

--- klipper/extras/config_switch.py
+++ klipper/extras/config_switch.py
@@ -14,17 +14,16 @@ class ConfigSwitch:
     cmd_SAVE_CONFIG_MODE_help = "..."
     def cmd_SAVE_CONFIG_MODE(self, gcmd):
         home_dir = os.path.expanduser("~")
-
         config_dir = os.path.join(home_dir, "printer_data/config/config-test/")
+        record_point = False
+
         if not os.path.exists(config_dir):
             os.makedirs(config_dir, exist_ok=True) 
 
-        # printer_config = os.path.join(home_dir, "printer_data/config/printer.cfg")
+        printer_config = os.path.join(home_dir, "printer_data/config/printer.cfg")
 
-        # tc_config_multi = os.path.join(storage_dir, f"tc_config_multi.cfg")
-        # tc_config_single = os.path.join(storage_dir, f"tc_config_single.cfg")
-
-        # record_point = False
+        tc_config_multi_file = os.path.join(config_dir, f"tc_config_multi.cfg")
+        tc_config_single_file = os.path.join(config_dir, f"tc_config_single.cfg")
         
         # self.gcode.respond_info("Record point:", record_point)
 
