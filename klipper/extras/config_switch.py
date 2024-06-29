4d938f49e7d39399351f4fe039595b1471101de7
VIN <71225264+VIN-y@users.noreply.github.com>
2024-06-29 12:23:23 +0100

    Update config_switch.py

--- klipper/extras/config_switch.py
+++ klipper/extras/config_switch.py
@@ -15,7 +15,7 @@ class ConfigSwitch:
     def cmd_SAVE_CONFIG_MODE(self, gcmd):
         home_dir = os.path.expanduser("~")
         config_dir = os.path.join(home_dir, "printer_data/config/config-test/")
-        record_point = False
+        record = False
 
         if not os.path.exists(config_dir):
             os.makedirs(config_dir, exist_ok=True) 
@@ -26,15 +26,14 @@ class ConfigSwitch:
         tc_config_single_file = os.path.join(config_dir, f"tc_config_single.cfg")
 
         with open(printer_config) as file:
-            self.gcode.respond_info("Record point: " + str(record_point))
             for line in file:
-                if "#;<" in line.strip() and record_point == False:
-                    record_point = True
-                    self.gcode.respond_info("Record point: " + str(record_point))
+                ## Record point begin / end
+                if "#;<" in line.strip() and record == False:
+                    record = True
+                if "#;>" in line.strip() and record == True :
+                    record = False
 
-            #     if "#;>" in string and record_point == True :
-            #         record_point = False
-            #         self.gcode.respond_info("Record point: " + str(record_point))
+                self.gcode.respond_info("Record point: " + str(record))
 
 
 def load_config(config):
