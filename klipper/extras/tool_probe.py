4406d9623d1c6b5af9a98a4f309cb86d277dc932
VIN <71225264+VIN-y@users.noreply.github.com>
2024-12-12 14:03:46 +0000

    Revert "Update tool_probe.py"
    This reverts commit 3e1ace4701432af8a717c1cebee9b3906411cb3d.

--- klipper/extras/tool_probe.py
+++ klipper/extras/tool_probe.py
@@ -3,22 +3,16 @@
 # Copyright (C) 2023 Viesturs Zarins <viesturz@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-#
-# Contribution 2024 by Justin F. Hallett <thesin@southofheaven.org>
 import logging
-from .probe import PrinterProbe, ProbeSessionHelper, ProbeEndstopWrapper, ProbeOffsetsHelper, calc_probe_z_average
+from . import probe
 
-class ToolProbe(PrinterProbe):
+class ToolProbe:
     def __init__(self, config):
         self.tool = config.getint('tool')
-        self.name = config.get_name()
-        
         self.printer = config.get_printer()
-        self.mcu_probe = ProbeEndstopWrapper(config)
-        # Handled in tool_probe_endstop
-        #self.cmd_helper = ProbeCommandHelper(config, self,
-        #                                     self.mcu_probe.query_endstop)
-        self.probe_offsets = ProbeOffsetsHelper(config)
+        self.name = config.get_name()
+        self.mcu_probe = probe.ProbeEndstopWrapper(config)
+        self.probe_offsets = probe.ProbeOffsetsHelper(config)
         self.probe_session = ProbeSessionHelper(config, self.mcu_probe)
 
         # Crash detection stuff
@@ -28,17 +22,23 @@ class ToolProbe(PrinterProbe):
         ppins.allow_multi_use_pin(pin.replace('^', '').replace('!', ''))
         buttons.register_buttons([pin], self._button_handler)
 
-        # Register with the endstop
+        #Register with the endstop
         self.endstop = self.printer.load_object(config, "tool_probe_endstop")
         self.endstop.add_probe(config, self)
+
     def _button_handler(self, eventtime, is_triggered):
         self.endstop.note_probe_triggered(self, eventtime, is_triggered)
 
+    def get_probe_params(self, gcmd=None):
+        return self.probe_session.get_probe_params(gcmd)
+    def get_offsets(self):
+        return self.probe_offsets.get_offsets()
+    def start_probe_session(self, gcmd):
+        return self.probe_session.start_probe_session(gcmd)
 
 # Helper to track multiple probe attempts in a single command
-class ProbeSessionHelper(ProbeSessionHelper):
+class ProbeSessionHelper:
     def __init__(self, config, mcu_probe):
-        self.drop_first_result = config.getboolean("drop_first_result", False)
         self.printer = config.get_printer()
         self.mcu_probe = mcu_probe
         gcode = self.printer.lookup_object('gcode')
@@ -52,8 +52,6 @@ class ProbeSessionHelper(ProbeSessionHelper):
             pconfig = config.getsection('printer')
             self.z_position = pconfig.getfloat('minimum_z_position', 0.,
                                                note_valid=False)
-        # Handled in tool_probe_endstop
-        # self.homing_helper = HomingViaProbeHelper(config, mcu_probe)
         # Configurable probing speeds
         self.speed = config.getfloat('speed', 5.0, above=0.)
         self.lift_speed = config.getfloat('lift_speed', self.speed, above=0.)
@@ -61,7 +59,7 @@ class ProbeSessionHelper(ProbeSessionHelper):
         self.sample_count = config.getint('samples', 1, minval=1)
         self.sample_retract_dist = config.getfloat('sample_retract_dist', 2.,
                                                    above=0.)
-        atypes = ['median', 'average']
+        atypes = {'median': 'median', 'average': 'average'}
         self.samples_result = config.getchoice('samples_result', atypes,
                                                'average')
         self.samples_tolerance = config.getfloat('samples_tolerance', 0.100,
@@ -74,7 +72,70 @@ class ProbeSessionHelper(ProbeSessionHelper):
         # Register event handlers
         self.printer.register_event_handler("gcode:command_error",
                                             self._handle_command_error)
-    def run_probe(self, gcmd, check_drop=True):
+    def _handle_command_error(self):
+        if self.multi_probe_pending:
+            try:
+                self.end_probe_session()
+            except:
+                logging.exception("Multi-probe end")
+    def _probe_state_error(self):
+        raise self.printer.command_error(
+            "Internal probe error - start/end probe session mismatch")
+    def start_probe_session(self, gcmd):
+        if self.multi_probe_pending:
+            self._probe_state_error()
+        self.mcu_probe.multi_probe_begin()
+        self.multi_probe_pending = True
+        self.results = []
+        return self
+    def end_probe_session(self):
+        if not self.multi_probe_pending:
+            self._probe_state_error()
+        self.results = []
+        self.multi_probe_pending = False
+        self.mcu_probe.multi_probe_end()
+    def get_probe_params(self, gcmd=None):
+        if gcmd is None:
+            gcmd = self.dummy_gcode_cmd
+        probe_speed = gcmd.get_float("PROBE_SPEED", self.speed, above=0.)
+        lift_speed = gcmd.get_float("LIFT_SPEED", self.lift_speed, above=0.)
+        samples = gcmd.get_int("SAMPLES", self.sample_count, minval=1)
+        sample_retract_dist = gcmd.get_float("SAMPLE_RETRACT_DIST",
+                                             self.sample_retract_dist, above=0.)
+        samples_tolerance = gcmd.get_float("SAMPLES_TOLERANCE",
+                                           self.samples_tolerance, minval=0.)
+        samples_retries = gcmd.get_int("SAMPLES_TOLERANCE_RETRIES",
+                                       self.samples_retries, minval=0)
+        samples_result = gcmd.get("SAMPLES_RESULT", self.samples_result)
+        return {'probe_speed': probe_speed,
+                'lift_speed': lift_speed,
+                'samples': samples,
+                'sample_retract_dist': sample_retract_dist,
+                'samples_tolerance': samples_tolerance,
+                'samples_tolerance_retries': samples_retries,
+                'samples_result': samples_result}
+    def _probe(self, speed):
+        toolhead = self.printer.lookup_object('toolhead')
+        curtime = self.printer.get_reactor().monotonic()
+        if 'z' not in toolhead.get_status(curtime)['homed_axes']:
+            raise self.printer.command_error("Must home before probe")
+        pos = toolhead.get_position()
+        pos[2] = self.z_position
+        try:
+            epos = self.mcu_probe.probing_move(pos, speed)
+        except self.printer.command_error as e:
+            reason = str(e)
+            if "Timeout during endstop homing" in reason:
+                reason += probe.HINT_TIMEOUT
+            raise self.printer.command_error(reason)
+        # Allow axis_twist_compensation to update results
+        self.printer.send_event("probe:update_results", epos)
+        # Report results
+        gcode = self.printer.lookup_object('gcode')
+        gcode.respond_info("probe at %.3f,%.3f is z=%.6f"
+                           % (epos[0], epos[1], epos[2]))
+        return epos[:3]
+    def run_probe(self, gcmd):
         if not self.multi_probe_pending:
             self._probe_state_error()
         params = self.get_probe_params(gcmd)
@@ -83,32 +144,30 @@ class ProbeSessionHelper(ProbeSessionHelper):
         retries = 0
         positions = []
         sample_count = params['samples']
-        first_probe = True
         while len(positions) < sample_count:
             # Probe position
             pos = self._probe(params['probe_speed'])
-            if check_drop and self.drop_first_result and first_probe:
-                gcmd.respond_info("dropping probe result, settling")
-            else:
-                positions.append(pos)
-                # Check samples tolerance
-                z_positions = [p[2] for p in positions]
-                if max(z_positions)-min(z_positions) > params['samples_tolerance']:
-                    if retries >= params['samples_tolerance_retries']:
-                        raise gcmd.error("Probe samples exceed samples_tolerance")
-                    gcmd.respond_info("Probe samples exceed tolerance. Retrying...")
-                    retries += 1
-                    positions = []
-            first_probe = False
+            positions.append(pos)
+            # Check samples tolerance
+            z_positions = [p[2] for p in positions]
+            if max(z_positions)-min(z_positions) > params['samples_tolerance']:
+                if retries >= params['samples_tolerance_retries']:
+                    raise gcmd.error("Probe samples exceed samples_tolerance")
+                gcmd.respond_info("Probe samples exceed tolerance. Retrying...")
+                retries += 1
+                positions = []
             # Retract
             if len(positions) < sample_count:
                 toolhead.manual_move(
                     probexy + [pos[2] + params['sample_retract_dist']],
                     params['lift_speed'])
         # Calculate result
-        epos = calc_probe_z_average(positions, params['samples_result'])
+        epos = probe.calc_probe_z_average(positions, params['samples_result'])
         self.results.append(epos)
-
+    def pull_probed_results(self):
+        res = self.results
+        self.results = []
+        return res
 
 def load_config_prefix(config):
     return ToolProbe(config)
