8e55e33e5298b3c4fd9d093365c5825875c75b0c
VIN <71225264+VIN-y@users.noreply.github.com>
2024-12-16 15:14:42 +0000

    Update toolchanger.py

--- klipper/extras/toolchanger.py
+++ klipper/extras/toolchanger.py
@@ -326,7 +326,8 @@ class Toolchanger:
             self.run_gcode('after_change_gcode',
                            after_change_gcode, extra_context)
 
-        self._restore_axis(gcode_position, restore_axis, tool)
+        # self._restore_axis(gcode_position, restore_axis, tool)
+        self._restore_axis(toolhead_position, restore_axis, tool)
 
         self.gcode.run_script_from_command(
             "RESTORE_GCODE_STATE NAME=_toolchange_state MOVE=0")
