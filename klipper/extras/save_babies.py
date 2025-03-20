f8851c29e2212c45caee4b6ad80d7a1197049714
VIN <71225264+VIN-y@users.noreply.github.com>
2025-03-20 22:39:58 +0000

    Update save_babies.py

--- klipper/extras/save_babies.py
+++ klipper/extras/save_babies.py
@@ -32,11 +32,8 @@ class SaveBabies:
                         for word in line.split():
                             if word != "#*#" and word != "z_offset" and word != "=":
                                 z_offset = float(word)
-                                
-                        with open(destination, 'a') as savefile:
-                            savefile.write(z_offset)
+                                self.gcode.respond_info("%f", z_offset)
 
-        self.gcode.respond_info("stuff_0...")
 
 def load_config(config):
     return SaveBabies(config)
