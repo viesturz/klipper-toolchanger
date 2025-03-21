5279cd006dda35ad528a70343b1e81c4493d7e4a
VIN <71225264+VIN-y@users.noreply.github.com>
2025-03-21 21:43:34 +0000

    Update save_babies.py

--- klipper/extras/save_babies.py
+++ klipper/extras/save_babies.py
@@ -15,12 +15,11 @@ class SaveBabies:
     cmd_SAVE_BABYSTEPS_help = "Save z-babysteps to printer.cfg"
     def cmd_SAVE_BABYSTEPS(self, gcmd):
         ## Variables
-        cur_tool = gcmd.get_float('CURTOOL', -1)
         z_offset = gcmd.get_float('OFFSET', 0.0)
         if cur_tool != -1 and z_offset != 0.0:
-            self.save_babysteps(gcmd, cur_tool, z_offset)
+            self.save_babysteps(gcmd, z_offset)
 
-    def save_babysteps(self, gcmd, active_tool, babystep):
+    def save_babysteps(self, gcmd, babystep):
         ## Variables
         home_dir = os.path.expanduser("~")
         printer_config = os.path.join(home_dir, "printer_data/config/printer_test.cfg")
@@ -35,20 +34,19 @@ class SaveBabies:
             with open(printer_config) as file:                    
                 for line in file:
                     if "#*# [tool_probe T" in line.strip():
-                        for word in line.split():
-                            if word != "#*#" and word != "[tool_probe" and word != "]":
-                                tool = str(word).replace("]", "")
-
+                        section = (line.replace("#*# [", "")).replace("]", "")
+ 
                     ## Calculate value
                     if "#*# z_offset =" in line.strip():
                         for word in line.split():
                             if word != "#*#" and word != "z_offset" and word != "=":
-                                z_offset = float(word) 
+                                z_offset = float(word)
 
-                        self.gcode.respond_info("Tool: %s | z_offset = %f" % (tool, z_offset))
+                    if tool and z_offset:
+                        self.gcode.respond_info("%s | z_offset = %f" % (section, z_offset))
 
-        # self.gcode.run_script_from_command("_CURRENT_OFFSET")
-        # self.gcode.run_script_from_command("TOOL_CALIBRATE_SAVE_TOOL_OFFSET SECTION="tool T{}" ATTRIBUTE=z_offset VALUE={}")
+            # self.gcode.run_script_from_command("_CURRENT_OFFSET")
+            # self.gcode.run_script_from_command("TOOL_CALIBRATE_SAVE_TOOL_OFFSET SECTION="tool T{}" ATTRIBUTE=z_offset VALUE={}")
 
 def load_config(config):
     return SaveBabies(config)
