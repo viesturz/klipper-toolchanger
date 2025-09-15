a5f2e33dfa77e6b6b87c3fc262a1bd75ba487483
VIN <71225264+VIN-y@users.noreply.github.com>
2025-09-15 08:31:42 +0100

    Update save_babies.py
    Update condition and add error message.

--- klipper/extras/save_babies.py
+++ klipper/extras/save_babies.py
@@ -32,7 +32,7 @@ class SaveBabies:
             with open(printer_config) as file:
                 for line in file:
                     ## find the section variable
-                    if "#*# [tool_probe" in line.strip():
+                    if "#*# [tool_probe " in line.strip():
                         section = ((line.replace("#*# [", "")).replace("]", "")).replace("\n", "")
                     
                     ## find the z_offset variable and apply the baby-step.
@@ -44,9 +44,11 @@ class SaveBabies:
                         
                         ## [printer.cfg] always checked for error on start-up. It can be reliably expected that the "section" variable is figured
                         ## out before the "current_z_offset" is determined. Therefore, there is no need for further checking function... I think.
-                        if section != "tool_probe_endstop":
+                        # if section != "tool_probe_endstop":
+                        if section is not None:
                             self.gcode.run_script_from_command("TOOL_CALIBRATE_SAVE_TOOL_OFFSET SECTION=\"%s\" ATTRIBUTE=z_offset VALUE=%f" % (section, z_offset))
                             self.gcode.respond_info("[%s] | z_offset = %f" % (section, z_offset))
-
+                        else:
+                            self.gcode.respond_error("No tool_probe detected")
 def load_config(config):
     return SaveBabies(config)
