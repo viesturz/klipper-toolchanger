99e683992fd302d7f4b44182cea514dfd970782e
VIN <71225264+VIN-y@users.noreply.github.com>
2025-09-25 23:19:21 +0100

    Update toolchanger.py

--- klipper/extras/toolchanger.py
+++ klipper/extras/toolchanger.py
@@ -332,8 +332,8 @@ class Toolchanger:
                 'restore_position': self._position_with_tool_offset(gcode_position, restore_axis, tool, extra_z_offset),
             }
 
-            self.gcode.run_script_from_command("SAVE_GCODE_STATE NAME=_toolchange_state")
             self.gcode.run_script_from_command("SET_GCODE_OFFSET X=0.0 Y=0.0 Z=0.0 MOVE=0")
+            self.gcode.run_script_from_command("SAVE_GCODE_STATE NAME=_toolchange_state")
 
             before_change_gcode = self.active_tool.before_change_gcode if self.active_tool and self.active_tool.before_change_gcode else self.default_before_change_gcode
             self.run_gcode('before_change_gcode', before_change_gcode, extra_context)
